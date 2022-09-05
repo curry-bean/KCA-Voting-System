7b51222224b488e070bdf66cb996c2714d1b95ae
curry-bean <70557443+curry-bean@users.noreply.github.com>
2022-09-05 15:24:47 -0400

    Update Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -10,6 +10,7 @@ import Slider from "react-slick";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AddToCartAction, RemoveFromCart } from "../../actions/cartActions";
+import React, { useState, useRef, useEffect } from 'react';
 
 export function numberWithComas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -60,11 +61,99 @@ const Home = () => {
                   fontSize: "35px",
                 }}
               />{" "}
-              Flash Sales
+              VOTE for your next leadership committee
             </span>
-            <span>
-              Time Left:<strong>11h:50m:45s</strong>{" "}
-            </span>
+
+
+	// We need ref in this, because we are dealing
+	// with JS setInterval to keep track of it and
+	// stop it when needed
+	const Ref = useRef(null);
+
+	// The state for our timer
+	const [timer, setTimer] = useState('00:00:00');
+
+
+	const getTimeRemaining = (e) => {
+		const total = Date.parse(e) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+		return {
+			total, hours, minutes, seconds
+		};
+	}
+
+
+	const startTimer = (e) => {
+		let { total, hours, minutes, seconds }
+					= getTimeRemaining(e);
+		if (total >= 0) {
+
+			// update the timer
+			// check if less than 10 then we need to
+			// add '0' at the beginning of the variable
+			setTimer(
+				(hours > 9 ? hours : '0' + hours) + ':' +
+				(minutes > 9 ? minutes : '0' + minutes) + ':'
+				+ (seconds > 9 ? seconds : '0' + seconds)
+			)
+		}
+	}
+
+
+	const clearTimer = (e) => {
+
+		// If you adjust it you should also need to
+		// adjust the Endtime formula we are about
+		// to code next
+		setTimer('00:00:10');
+
+		// If you try to remove this line the
+		// updating of timer Variable will be
+		// after 1000ms or 1sec
+		if (Ref.current) clearInterval(Ref.current);
+		const id = setInterval(() => {
+			startTimer(e);
+		}, 1000)
+		Ref.current = id;
+	}
+
+	const getDeadTime = () => {
+		let deadline = new Date();
+
+		// This is where you need to adjust if
+		// you entend to add more time
+		deadline.setSeconds(deadline.getSeconds() + 10);
+		return deadline;
+	}
+
+	// We can use useEffect so that when the component
+	// mount the timer will start as soon as possible
+
+	// We put empty array to act as componentDid
+	// mount only
+	useEffect(() => {
+		clearTimer(getDeadTime());
+	}, []);
+
+	// Another way to call the clearTimer() to start
+	// the countdown is via action event from the
+	// button first we create function to be called
+	// by the button.
+	return (
+		<div style={{
+        display:'flex',
+        }}
+    >
+			<h2>{timer}</h2>
+			
+		</div>
+	)
+
+
+
+
           </div>
           {products.map((product) => {
             return (
@@ -134,7 +223,7 @@ const Home = () => {
             alignItems: "center",
           }}
         >
-          Fast Delivery Within Nairobi
+          Advancing Knowledge, Driving Change
         </span>
       </div>
     </>
