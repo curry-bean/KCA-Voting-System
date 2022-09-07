f334e8b123f410ec2f5d16c17b0047c8f5024b20
curry-bean <70557443+curry-bean@users.noreply.github.com>
2022-09-07 03:48:32 -0400

    Update Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,4 @@
-
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Products } from "../../actions/productsActions";
 import ErrorMessage from "../../helpers/ErrorMessage";
